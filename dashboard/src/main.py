7ca66b58e5ee441826e74ea1773668d009e30de7
KZN-coder <deal_dragunov@mail.ru>
2025-04-25 01:40:59 +0300

    Project refactoring

--- dashboard/src/main.py
+++ dashboard/src/main.py
@@ -4,18 +4,8 @@ from .http_client import CMCHTTPClient
 
 from dashboard.src.config import settings
 
-app = FastAPI(
+from .router import router as router_crypto
 
-)
-cmc_client = CMCHTTPClient(
-    "https://pro-api.coinmarketcap.com",
-    settings.CMC_API_KEY,
-)
+app = FastAPI()
 
-@app.get("/cryptocurrencies")
-async def get_cryptocurrencies():
-    return await cmc_client.get_listings()
-
-@app.get("/cryptocurrencies/{currency_id}")
-async def get_cryptocyrrecy(currency_id: int):
-    return await cmc_client.get_currency(currency_id=currency_id)
+app.include_router(router_crypto)
