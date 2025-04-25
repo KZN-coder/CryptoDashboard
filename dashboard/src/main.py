a47205eae4ce37cd2aac4b7fe6052dc4b7b75849
KZN-coder <deal_dragunov@mail.ru>
2025-04-25 03:44:03 +0300

    Updated CORS

--- dashboard/src/main.py
+++ dashboard/src/main.py
@@ -6,6 +6,21 @@ from dashboard.src.config import settings
 
 from .router import router as router_crypto
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router_crypto)
